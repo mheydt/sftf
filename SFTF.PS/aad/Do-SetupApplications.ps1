e1b7456fd0a81968eb631d82b7ab9b6c4db27a5b
Mike <mike@heydt.org>
2017-02-15 08:36:46 -0800

    Restructuring

--- SFTF.PS/aad/Do-SetupApplications.ps1
+++ SFTF.PS/aad/Do-SetupApplications.ps1
@@ -2,12 +2,15 @@
 # Do_SetupApplications.ps1
 #
 
+# run this from cmd line
+
 $yourTenantId = "72f988bf-86f1-41af-91ab-2d7cd011db47"
 $yourClusterName = "sfhack"
 $region = "us-west"
 $clusterDNS = $yourClusterName + "." + $region + ".cloudapp.azure.com"
 $webAppUrl = "https://" + $clusterDNS + ":19080/Explorer/index.html"
 
-.\Setup-Applications.ps1 `
-	-TenantId $yourTenantId -ClusterName $clusterDNS `
-	-WebApplicationReplyUrl ‘https://<your-cluster-name>.<region>.cloudapp.azure.com:19080/Explorer/index.html‘
+. $PSScriptRoot\Setup-Applications.ps1 `
+	-TenantId $yourTenantId `
+	-ClusterName $clusterDNS `
+	-WebApplicationReplyUrl ‘https://' + $clusterDNS + ":19080/Explorer/index.html"
